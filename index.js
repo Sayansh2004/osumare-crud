d041b7506d03c770d6b5d60af14ae50af22f0c3c
Sayansh Chopra <choprasayansh@gmail.com>
2025-05-08 12:04:29 +0530

    Add start script and update package.json

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { v4: uuidv4}=require('uuid');
 const methodOverride = require("method-override");
 const ExpressError = require('./expressError');
 
+
 app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
